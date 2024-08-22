ad23434ed2437ff6c94b8db25f0438ad24838624
Felipe Ferreira de Paula <122496288+felipefp22@users.noreply.github.com>
2024-08-22 16:48:27 -0700

    Initial commit

---
+++ src/main/java/com/SharedCheksMercadoPagoIntegration/SharedCheksMercadoPagoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.SharedCheksMercadoPagoIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedCheksMercadoPagoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedCheksMercadoPagoIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SharedCheksMercadoPagoIntegration/SharedCheksMercadoPagoIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SharedCheksMercadoPagoIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedCheksMercadoPagoIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
